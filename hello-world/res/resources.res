3e84ea9046557ff73b279dcf34ab6a24f391968e
diegomtassis <diego.mt.dev@gmail.com>
2019-03-20 22:51:56 +0100

    first tests using tiles

---
+++ hello-world/res/resources.res
@@ -0,0 +1,2 @@
+IMAGE moon "moon.bmp" 0
+TILESET platform "jetpac-platform.bmp" 0
--- hello-world/src/main.c
+++ hello-world/src/main.c
@@ -3,7 +3,11 @@
  */
 
 #include <genesis.h>
-#include <input-handling.h>
+
+#include "../inc/input-handling.h"
+#include "../res/resources.h"
+
+#define TILE1	1
 
 int main() {
 
@@ -11,6 +15,24 @@ int main() {
 	JOY_setEventHandler(&inputHandler);
 
 	VDP_drawText("Hello Genny World!", 10, 5);
+
+	// Moon
+	VDP_setPalette(PAL1, moon.palette->data);
+	VDP_drawImageEx(PLAN_A, &moon, TILE_ATTR_FULL(PAL1, 0, 0, 0, 1), 12, 12, 0, CPU);
+
+
+	// Platform
+	VDP_loadTileData(platform.tiles, 1, 3, 0);
+
+	int minX = 5;
+	int maxX = 30;
+	int y = 25;
+
+	VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL2, 1, 0, 0, 1), minX, y);
+	for (int var = 1; var < maxX; ++var) {
+		VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL2, 1, 0, 0, 2), minX + var, y);
+	}
+	VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL2, 1, 0, 0, 3), minX + maxX, y);
 
 	while (1) {
 
