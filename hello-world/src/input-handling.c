040f6ee8a004385e83654c590b1be817c19d0c3d
diegomtassis <diego.mt.dev@gmail.com>
2019-03-20 10:33:29 +0100

    simplified input handling

--- hello-world/src/input-handling.c
+++ hello-world/src/input-handling.c
@@ -7,9 +7,11 @@
 
 #include <genesis.h>
 
+void logButtonStatus(const char *str, const char *isPushed, u16 x, u16 y);
+
 void inputHandler(u16 joy, u16 changed, u16 state) {
 
-	logInputButton(joy, BUTTON_START, changed, state, "START", 5, 12);
+	logInputButton(joy, BUTTON_START, changed, state, "S", 5, 12);
 
 	logInputButton(joy, BUTTON_A, changed, state, "A", 5, 14);
 	logInputButton(joy, BUTTON_B, changed, state, "B", 5, 15);
@@ -19,23 +21,28 @@ void inputHandler(u16 joy, u16 changed, u16 state) {
 	logInputButton(joy, BUTTON_Y, changed, state, "Y", 5, 19);
 	logInputButton(joy, BUTTON_Z, changed, state, "Z", 5, 20);
 
-	logInputButton(joy, BUTTON_UP, changed, state, "UP", 5, 22);
-	logInputButton(joy, BUTTON_DOWN, changed, state, "DOWN", 5, 23);
-	logInputButton(joy, BUTTON_LEFT, changed, state, "LEFT", 5, 24);
-	logInputButton(joy, BUTTON_RIGHT, changed, state, "RIGHT", 5, 25);
+	logInputButton(joy, BUTTON_UP, changed, state, "U", 5, 22);
+	logInputButton(joy, BUTTON_DOWN, changed, state, "D", 5, 23);
+	logInputButton(joy, BUTTON_LEFT, changed, state, "L", 5, 24);
+	logInputButton(joy, BUTTON_RIGHT, changed, state, "R", 5, 25);
 }
 
 void logInputButton(u16 joy, u16 button, u16 changed, u16 state, const char *str, u16 x, u16 y) {
 
-	char buf[strlen(str) + 10];
-
 	if (joy == JOY_1) {
+
 		if (state & button) {
-			strcat(strcat(buf, str), " button: 1");
+			logButtonStatus(str, "1", x, y);
 
 		} else if (changed & button) {
-			strcat(strcat(buf, str), " button: 0");
+			logButtonStatus(str, "0", x, y);
 		}
-		VDP_drawText(buf, x, y);
 	}
+}
+
+void logButtonStatus(const char *str, const char *isPushed, u16 x, u16 y) {
+
+	char buf[strlen(str) + 3];
+	strcat(strcat(buf, str), isPushed);
+	VDP_drawText(buf, x, y);
 }
